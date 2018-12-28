2ddec863dbc68234ea323790d715624b699d9cfe
amirphl <amirphl@aut.ac.ir>
2018-12-29 02:00:33 +0330

    first stable version of tables.

--- src/create_tables.sql
+++ src/create_tables.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE OnlineStore;
-USE OnlineStore;
+# CREATE DATABASE OnlineStore;
+# USE OnlineStore;
 
 CREATE TABLE Customers (
   username    VARCHAR(100) PRIMARY KEY,
@@ -9,17 +9,18 @@ CREATE TABLE Customers (
   last_name   VARCHAR(100),
   postal_code CHAR(10)     NOT NULL,
   gender      ENUM ('man', 'woman'),
-  credit      INT UNSIGNED NOT NULL DEFAULT 0
+  credit      INT UNSIGNED NOT NULL DEFAULT 0,
+  CHECK (credit >= 0)
 );
 
 CREATE TABLE TemporaryCustomers (
   email        VARCHAR(150) PRIMARY KEY,
   first_name   VARCHAR(100),
   last_name    VARCHAR(100),
-  postal_code  CHAR(10)                            NOT NULL,
-  gender       ENUM ('man', 'woman') DEFAULT 'man' NOT NULL,
-  address      VARCHAR(200)                        NOT NULL,
-  phone_number VARCHAR(14)                         NOT NULL
+  postal_code  CHAR(10)     NOT NULL,
+  gender       ENUM ('man', 'woman') DEFAULT 'man',
+  address      VARCHAR(200) NOT NULL,
+  phone_number VARCHAR(14)  NOT NULL
 );
 
 
@@ -56,22 +57,29 @@ CREATE TABLE Product (
   title  VARCHAR(100)               NOT NULL,
   price  INTEGER UNSIGNED           NOT NULL,
   value  INTEGER UNSIGNED DEFAULT 1 NOT NULL,
-  offer  INTEGER UNSIGNED DEFAULT 0,
+  offer  FLOAT UNSIGNED DEFAULT 0,
   PRIMARY KEY (id, shopId),
   FOREIGN KEY (shopId) REFERENCES Shop (id)
     ON DELETE CASCADE
 );
 
 CREATE TABLE CustomerOrders (
-  id               CHAR(20),
+  id               INT AUTO_INCREMENT,
   customerUsername VARCHAR(100),
   shopId           CHAR(20),
   productId        CHAR(20),
-  value            INTEGER UNSIGNED DEFAULT 1                                                                 NOT NULL,
-  status           ENUM ('accepted', 'rejected', 'sending', 'done') DEFAULT 'rejected'                        NOT NULL,
-  payment_type     ENUM ('online', 'offline') DEFAULT 'online'                                                NOT NULL,
-  dat              TIMESTAMP DEFAULT current_timestamp                                                        NOT NULL,
-  address          VARCHAR(200)                                                                               NOT NULL,
+  value            INTEGER UNSIGNED DEFAULT 1                                                                  NOT NULL,
+  status           ENUM ('accepted', 'rejected', 'sending', 'done') DEFAULT 'accepted'                         NOT NULL,
+  payment_type     ENUM ('online', 'offline') DEFAULT 'online'                                                 NOT NULL,
+  dat              TIMESTAMP DEFAULT current_timestamp                                                         NOT NULL,
+  address          VARCHAR(200)                                                                                NOT NULL,
+  phone_number     VARCHAR(14)                                                                                 NOT NULL,
+  CHECK (address IN (SELECT CA.address
+                     FROM CustomerAddresses AS CA
+                     WHERE customerUsername = CA.CustomerUsername)),
+  CHECK (phone_number IN (SELECT CP.phone_number
+                          FROM CustomerPhoneNumbers AS CP
+                          WHERE customerUsername = CP.CustomerUsername)),
   PRIMARY KEY (id),
   FOREIGN KEY (customerUsername) REFERENCES Customers (username),
   FOREIGN KEY (shopId) REFERENCES Shop (id),
@@ -79,15 +87,13 @@ CREATE TABLE CustomerOrders (
 );
 
 CREATE TABLE TemporaryCustomerOrders (
-  id            CHAR(20),
+  id            INT AUTO_INCREMENT,
   customerEmail VARCHAR(150),
   shopId        CHAR(20),
   productId     CHAR(20),
-  value         INTEGER UNSIGNED DEFAULT 1                                          NOT NULL,
-  status        ENUM ('accepted', 'rejected', 'sending', 'done') DEFAULT 'rejected' NOT NULL,
-  payment_type  ENUM ('offline') DEFAULT 'offline'                                  NOT NULL,
-  dat           TIMESTAMP DEFAULT current_timestamp                                 NOT NULL,
-  address       VARCHAR(200)                                                        NOT NULL,
+  value         INTEGER UNSIGNED DEFAULT 1                                           NOT NULL,
+  status        ENUM ('accepted', 'rejected', 'sending', 'done') DEFAULT 'accepted'  NOT NULL,
+  dat           TIMESTAMP DEFAULT current_timestamp                                  NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (customerEmail) REFERENCES TemporaryCustomers (email),
   FOREIGN KEY (shopId) REFERENCES Shop (id),
@@ -124,4 +130,20 @@ CREATE TABLE Transmitters (
   credit       INTEGER UNSIGNED DEFAULT 0                 NOT NULL,
   PRIMARY KEY (id, shopId),
   FOREIGN KEY (shopId) REFERENCES Shop (id)
+);
+
+CREATE TABLE Shipment (
+  transmitterId CHAR(20),
+  orderId       INT,
+  PRIMARY KEY (transmitterId, orderId),
+  FOREIGN KEY (transmitterId) REFERENCES Transmitters (id),
+  FOREIGN KEY (orderId) REFERENCES CustomerOrders (id)
+);
+
+CREATE TABLE TemporaryShipment (
+  transmitterId CHAR(20),
+  orderId       INT,
+  PRIMARY KEY (transmitterId, orderId),
+  FOREIGN KEY (transmitterId) REFERENCES Transmitters (id),
+  FOREIGN KEY (orderId) REFERENCES TemporaryCustomerOrders (id)
 );
