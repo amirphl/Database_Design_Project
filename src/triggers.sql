350e6d5201d747d442b8a6d2cb150248c32135c9
amirphl <amirphl@aut.ac.ir>
2018-12-29 10:58:48 +0330

    add 'log_update_on_customer' trigger.

--- src/triggers.sql
+++ src/triggers.sql
@@ -11,3 +11,10 @@ FOR EACH ROW
   BEGIN
     CALL deliver_temporary_customer_order_to_transmitter(NEW.id, NEW.shopId);
   END;
+
+CREATE TRIGGER log_update_on_customers
+AFTER UPDATE ON customers
+FOR EACH ROW
+  BEGIN
+    INSERT INTO updatecustomerlog (username, dat) VALUES (NEW.username, current_timestamp);
+  END;
