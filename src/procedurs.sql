256916add979e1f1a2c0b4cf5583a8ec0276272b
amirphl <amirphl@aut.ac.ir>
2019-01-17 11:09:56 +0330

    fix issues of procedurs, issue of session variables, ... .

--- src/procedurs.sql
+++ src/procedurs.sql
@@ -1,3 +1,4 @@
+USE store;
 CREATE PROCEDURE add_customer(IN us VARCHAR(100), #username
                               IN pa VARCHAR(30), #password
                               IN em VARCHAR(150), #email
@@ -8,55 +9,45 @@ CREATE PROCEDURE add_customer(IN us VARCHAR(100), #username
                               IN cr INT UNSIGNED # credit
 )
   BEGIN
-    DECLARE `_rollback` BOOL DEFAULT 0;
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `_rollback` = 1;
-    START TRANSACTION;
     INSERT INTO customers (username, password, email, first_name, last_name, postal_code, gender, credit)
-    VALUES (us, sha2(pa, 256), em, fi, la, po, ge, cr);
-    IF `_rollback`
-    THEN
-      ROLLBACK;
-    ELSE
-      COMMIT;
-    END IF;
+    VALUES (us, sha1(pa), em, fi, la, po, ge, cr);
   END;
 
-CREATE PROCEDURE update_customer(IN us VARCHAR(100), #username
-                                 IN pa VARCHAR(30), #password
-                                 IN em VARCHAR(150), #email
-                                 IN fi VARCHAR(100), #first_name
-                                 IN la VARCHAR(100), #last_name
-                                 IN po CHAR(10), #postal_code
-                                 IN ge ENUM ('man', 'woman')#gender
-)
-  BEGIN
-    UPDATE customers
-    SET password = sha2(pa, 256), email = em, first_name = fi, last_name = la, postal_code = po, gender = ge
-    WHERE username = us;
-  END;
+# CREATE PROCEDURE update_customer(IN us VARCHAR(100), #username
+#                                  IN pa VARCHAR(30), #password
+#                                  IN em VARCHAR(150), #email
+#                                  IN fi VARCHAR(100), #first_name
+#                                  IN la VARCHAR(100), #last_name
+#                                  IN po CHAR(10), #postal_code
+#                                  IN ge ENUM ('man', 'woman')#gender
+# )
+#   BEGIN
+#     UPDATE customers
+#     SET password = sha1(pa), email = em, first_name = fi, last_name = la, postal_code = po, gender = ge
+#     WHERE username = us;
+#   END;
 
-CREATE PROCEDURE add_order_by_customer(IN cu VARCHAR(100), #customerUsername
-                                       IN sh CHAR(20), #shopId
-                                       IN pr CHAR(20), # productId
+# This is an example of a bullshit query, remember, never use session variable, instead, use procedure variable(local variable)
+# www.stackoverflow.com/questions/1009954/mysql-variable-vs-variable-whats-the-difference
+CREATE PROCEDURE add_order_by_customer(IN cu VARCHAR(100), # customerUsername
+                                       IN sh INT UNSIGNED, # shopId
+                                       IN pr INT UNSIGNED, # productId
                                        IN va INTEGER, # value
                                        IN pa ENUM ('online', 'offline'), # payment_type
                                        IN ad VARCHAR(200), # address
                                        IN ph VARCHAR(14) # phone_number
 )
   BEGIN
-    DECLARE `_rollback` BOOL DEFAULT 0;
-    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `_rollback` = 1;
-    START TRANSACTION;
 
-    SELECT @pri := P.price
+    SELECT @price_of_product := P.price
     FROM product AS P
     WHERE P.shopId = sh AND P.id = pr;
 
-    SELECT @offe := P.offer
+    SELECT @offer_of_product := P.offer
     FROM product AS P
     WHERE P.shopId = sh AND P.id = pr;
 
-    SELECT @valu := P.value
+    SELECT @supply_of_product := P.value
     FROM product AS P
     WHERE P.shopId = sh AND P.id = pr;
 
@@ -72,15 +63,18 @@ CREATE PROCEDURE add_order_by_customer(IN cu VARCHAR(100), #customerUsername
     FROM shop AS S
     WHERE S.id = sh;
 
-    IF @valu < va OR @cu_cred < ((1.0 - @offe) * @pri * va) OR @shop_start_time > current_time OR
+    IF @supply_of_product < va OR @cu_cred < ((1.0 - @offer_of_product) * @price_of_product * va) OR
+       @shop_start_time > current_time OR
        current_time > @shop_end_time
     THEN
+
       INSERT INTO customerorders (customerUsername, shopId, productId, value, status, payment_type, address, phone_number)
         VALUE (cu, sh, pr, va, 'rejected', pa, ad, ph);
+
     ELSE
 
       UPDATE customers AS C
-      SET C.credit = C.credit - (1.0 - @offe) * @pri * va
+      SET C.credit = C.credit - (1.0 - @offer_of_product) * @price_of_product * va
       WHERE C.username = cu AND pa = 'online';
 
       UPDATE product AS P
@@ -89,19 +83,16 @@ CREATE PROCEDURE add_order_by_customer(IN cu VARCHAR(100), #customerUsername
 
       INSERT INTO customerorders (customerUsername, shopId, productId, value, payment_type, address, phone_number)
         VALUE (cu, sh, pr, va, pa, ad, ph);
+
     END IF;
 
-    IF `_rollback`
-    THEN
-      ROLLBACK;
-    ELSE
-      COMMIT;
-    END IF;
   END;
 
-CREATE PROCEDURE add_order_by_temporary_customer(IN cu VARCHAR(150), #customerEmail
-                                                 IN sh CHAR(20), #shop id
-                                                 IN pr CHAR(20), # product id
+# This is an example of a bullshit query, remember, never use session variable, instead, use procedure variable(local variable)
+# www.stackoverflow.com/questions/1009954/mysql-variable-vs-variable-whats-the-difference
+CREATE PROCEDURE add_order_by_temporary_customer(IN cu VARCHAR(150), # customerEmail
+                                                 IN sh INT UNSIGNED, # shop id
+                                                 IN pr INT UNSIGNED, # product id
                                                  IN va INTEGER # value
 )
   BEGIN
@@ -109,7 +100,7 @@ CREATE PROCEDURE add_order_by_temporary_customer(IN cu VARCHAR(150), #customerEm
     DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `_rollback` = 1;
     START TRANSACTION;
 
-    SELECT @valu := P.value
+    SELECT @supply_of_product := P.value
     FROM product AS P
     WHERE P.shopId = sh AND P.id = pr;
 
@@ -121,13 +112,16 @@ CREATE PROCEDURE add_order_by_temporary_customer(IN cu VARCHAR(150), #customerEm
     FROM shop AS S
     WHERE S.id = sh;
 
-    IF @shop_start_time <= current_time AND current_time <= @shop_end_time AND @valu >= va
+    IF @shop_start_time <= current_time AND current_time <= @shop_end_time AND @supply_of_product >= va
     THEN
+
       INSERT INTO temporarycustomerorders (customerEmail, shopId, productId, value)
         VALUE (cu, sh, pr, va);
+
       UPDATE product AS P
       SET P.value = P.value - va
       WHERE P.id = pr AND P.shopId = sh;
+
     ELSE
       INSERT INTO temporarycustomerorders (customerEmail, shopId, productId, value, status)
         VALUE (cu, sh, pr, va, 'rejected');
@@ -141,97 +135,60 @@ CREATE PROCEDURE add_order_by_temporary_customer(IN cu VARCHAR(150), #customerEm
     END IF;
   END;
 
-CREATE PROCEDURE deliver_customer_order_to_transmitter(IN oid INT, # orderId
-                                                       IN sh  CHAR(20) # shopId
+CREATE PROCEDURE deliver_to_customer(tid INT UNSIGNED, # transmitterID
+                                     pt  TIMESTAMP, # purchase_time
+                                     cu  VARCHAR(100), # customerUsername
+                                     sh  INT UNSIGNED, # shopId
+                                     pr  INT UNSIGNED # productId
 )
   BEGIN
-    SELECT @transmitter := T.id
-    FROM transmitters AS T
-    WHERE T.status = 'free' AND T.shopId = sh
-    LIMIT 1;
+    DECLARE state ENUM ('accepted', 'rejected', 'sending', 'done');
 
-    IF @transmitter IS NULL
+    SELECT status
+    INTO state
+    FROM customerorders AS C
+    WHERE C.purchase_time = pt AND C.customerUsername = cu AND C.shopId = sh AND C.productId = pr;
+
+    IF state != 'done'
     THEN
       UPDATE customerorders AS C
-      SET status = 'rejected'
-      WHERE C.id = oid;
-    ELSE
+      SET C.status = 'done'
+      WHERE C.purchase_time = pt AND C.customerUsername = cu AND C.shopId = sh AND C.productId = pr;
+
       UPDATE transmitters AS T
-      SET status = 'sending'
-      WHERE T.id = @transmitter;
-
-      UPDATE customerorders AS C
-      SET status = 'sending'
-      WHERE C.id = oid;
-
-      INSERT INTO shipment (transmitterId, orderId) VALUES (@transmitter, oid);
+      SET T.status = 'free', T.credit = T.credit + 0.05 * (SELECT P.price
+                                                           FROM product AS P
+                                                           WHERE P.id = pr AND P.shopId = sh)
+      WHERE T.id = tid AND T.shopId = sh;
     END IF;
   END;
 
-CREATE PROCEDURE deliver_temporary_customer_order_to_transmitter(IN oid INT, # orderId
-                                                                 IN sh  CHAR(20) # shopId
+CREATE PROCEDURE deliver_to_temporary_customer(tid INT UNSIGNED, # transmitterID
+                                               pt  TIMESTAMP, # purchase_time
+                                               ce  VARCHAR(100), # customerEmail
+                                               sh  INT UNSIGNED, # shopId
+                                               pr  INT UNSIGNED # productId
 )
   BEGIN
-    SELECT @transmitter := T.id
-    FROM transmitters AS T
-    WHERE T.status = 'free' AND T.shopId = sh
-    LIMIT 1;
+    DECLARE state ENUM ('accepted', 'rejected', 'sending', 'done');
 
-    IF @transmitter IS NULL
+    SELECT status
+    INTO state
+    FROM temporarycustomerorders AS C
+    WHERE C.purchase_time = pt AND C.customerEmail = ce AND C.shopId = sh AND C.productId = pr;
+
+    IF state != 'done'
     THEN
       UPDATE temporarycustomerorders AS C
-      SET status = 'rejected'
-      WHERE C.id = oid;
-    ELSE
+      SET C.status = 'done'
+      WHERE C.purchase_time = pt AND C.customerEmail = ce AND C.shopId = sh AND C.productId = pr;
+
       UPDATE transmitters AS T
-      SET status = 'sending'
-      WHERE T.id = @transmitter;
-
-      UPDATE temporarycustomerorders AS C
-      SET status = 'sending'
-      WHERE C.id = oid;
-
-      INSERT INTO temporaryshipment (transmitterId, orderId) VALUES (@transmitter, oid);
+      SET T.status = 'free', T.credit = T.credit + 0.05 * (SELECT P.price
+                                                           FROM product AS P
+                                                           WHERE P.id = pr AND P.shopId = sh)
+      WHERE T.id = tid AND T.shopId = sh;
     END IF;
-  END;
-
-
-CREATE PROCEDURE deliver_to_customer(IN oid INT, # orderId
-                                     IN tid CHAR(20) #transmitterId
-)
-  BEGIN
-    UPDATE customerorders AS C
-    SET C.status = 'done'
-    WHERE C.id = oid;
-
-    SELECT @pid := productId
-    FROM customerorders AS C
-    WHERE C.id = oid;
-
-    UPDATE transmitters AS T
-    SET T.status = 'free', T.credit = T.credit + 0.05 * (SELECT P.price
-                                                         FROM product AS P
-                                                         WHERE P.id = @pid)
-    WHERE T.id = tid;
-  END;
-
-CREATE PROCEDURE deliver_to_temporary_customer(IN oid INT, # orderId
-                                               IN tid CHAR(20) #transmitterId
-)
-  BEGIN
-    UPDATE temporarycustomerorders AS C
-    SET C.status = 'done'
-    WHERE C.id = oid;
-
-    SELECT @pid := productId
-    FROM customerorders AS C
-    WHERE C.id = oid;
-
-    UPDATE transmitters AS T
-    SET T.status = 'free', T.credit = T.credit + 0.05 * (SELECT P.price
-                                                         FROM product AS P
-                                                         WHERE P.id = @pid)
-    WHERE T.id = tid;
   END;
 
 CREATE PROCEDURE charge_account(IN us VARCHAR(100), #username
